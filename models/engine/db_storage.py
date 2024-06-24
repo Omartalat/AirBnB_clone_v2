ad0d458caf83a05a02e342600472442c98315cdb
dr.omartalat <dr.omartalat@gmail.com>
2024-06-25 01:24:58 +0300

    db_storage modification

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """New engine"""
 import os
-from models.amenity import Amenity
-from models.review import Review
-from models.state import State
-from models.base_model import Base
-from models.city import City
-from models.place import Place
-from models.user import User
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class DBStorage:
@@ -18,48 +18,33 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        """Initialize a new DBStorage instance."""
-        mysql_user = os.environ.get("HBNB_MYSQL_USER")
-        mysql_passwd = os.environ.get("HBNB_MYSQL_PWD")
-        mysql_host = os.environ.get("HBNB_MYSQL_HOST")
-        mysql_db = os.environ.get("HBNB_MYSQL_DB")
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-            mysql_user, mysql_passwd, mysql_host, mysql_db),
-              pool_pre_ping=True)
+        """initialize a new DBStorage instance."""
+        user = os.getenv('HBNB_MYSQL_USER')
+        password = os.getenv('HBNB_MYSQL_PWD')
+        host = os.getenv('HBNB_MYSQL_HOST', 'localhost')
+        database = os.getenv('HBNB_MYSQL_DB')
+        hbnb_env = os.getenv('HBNB_ENV')
 
-        if os.environ.get('HBNB_ENV') == 'test':
+        self.__engine = create_engine(f'mysql+mysqldb://{user}:{password}@{host}/{database}', pool_pre_ping=True)
+
+        if hbnb_env == 'test':
             Base.metadata.drop_all(self.__engine)
 
-
     def all(self, cls=None):
-        """Query all objects depending on the class name (argument cls)"""
+        """Query all objects depending of the class name (argument cls)"""
         obj_dict = {}
-        try:
-            class_dict = {
-                "State": State,
-                "City": City,
-                "User": User,
-                "Place": Place,
-                "Review": Review,
-                "Amenity": Amenity
-            }
-
-            if cls:
-                if isinstance(cls, str):
-                    cls = class_dict.get(cls)
-                    if not cls:
-                        raise ValueError(f"Class '{cls}' is not defined.")
-                objs = self.__session.query(cls).all()
-            else:
-                objs = []
-                for class_ in class_dict.values():
-                    objs.extend(self.__session.query(class_).all())
-
+        if cls:
+            objs = self.__session.query(cls).all()
             for obj in objs:
-                key = '{}.{}'.format(type(obj).__name__, obj.id)
+                key = f"{obj.__class__.__name__}.{obj.id}"
                 obj_dict[key] = obj
-        except Exception as e:
-            print(f"An error occurred: {e}")
+        else:
+            classes = [User, State, City, Amenity, Place, Review]
+            for cls in classes:
+                objs = self.__session.query(cls).all()
+                for obj in objs:
+                    key = f"{obj.__class__.__name__}.{obj.id}"
+                    obj_dict[key] = obj
         return obj_dict
 
     def new(self, obj):
@@ -81,8 +66,6 @@ class DBStorage:
         2. create the current database session (self.__session)
         """
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory=session_factory)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
         self.__session = Session()
-        return self.__session
