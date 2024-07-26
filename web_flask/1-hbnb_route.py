3f244e70a5615990bb027e1df854a91235f226fd
Talat <dr.omartalat@gmail.com>
2024-07-26 22:29:38 +0000

    1-hbnb_route.py documentations for func added

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,13 +9,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def homepage():
+	"""display "Hello HBNB!""""
 	return "Hello HBNB!"
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
+	"""display "HBNB""""
 	return "HBNB"
 
 
