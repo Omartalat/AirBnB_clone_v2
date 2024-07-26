b4ba1ae5e8c245113eed65fbe337fc80b736ddc0
Talat <dr.omartalat@gmail.com>
2024-07-26 22:39:06 +0000

    pycodestyle for 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,15 +12,15 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def homepage():
-    """display "Hello HBNB!""""
-    return "Hello HBNB!"
+	"""display "Hello HBNB!""""
+	return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
-    """display "HBNB""""
-    return "HBNB"
+	"""display "HBNB""""
+	return "HBNB"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000)
