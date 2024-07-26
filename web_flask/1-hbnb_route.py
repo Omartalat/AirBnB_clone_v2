41cf1b76cbcf84e7daee7f3ea2f48e68ff159455
Talat <dr.omartalat@gmail.com>
2024-07-26 22:47:58 +0000

    pycodestyle for 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-a script that starts a Flask web application:
+A script that starts a Flask web application:
     /: display "Hello HBNB!"
     /hbnb: display "HBNB"
 """
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def homepage():
-    """display "Hello HBNB!""""
+    """display "Hello HBNB!" """
     return "Hello HBNB!"
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_route():
-    """display "HBNB""""
+    """display "HBNB" """
     return "HBNB"
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
