3192e0b2b55f96c1b21f428d130e72ea8db3f04b
Talat <dr.omartalat@gmail.com>
2024-07-27 14:48:45 +0000

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+	"""display "Hello HBNB!" """
+	return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def route_to_hbnb():
+	"""display "HBNB" """
+	return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def route_to_c(text):
+	"""display 'C'  followed by the value of the text variable"""
+	text = text.replace('_', ' ')
+	return ("C {}".format(text))
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
