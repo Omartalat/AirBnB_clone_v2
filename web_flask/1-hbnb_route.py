23efd8c884a7fc431a6d46a422e097f9bbb35ebb
dr.omartalat <dr.omartalat@gmail.com>
2024-07-27 23:11:33 +0300

    6-number_odd_or_even.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,6 +4,7 @@ A script that starts a Flask web application:
     /: display "Hello HBNB!"
     /hbnb: display "HBNB"
 """
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -23,4 +24,3 @@ def hbnb_route():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
